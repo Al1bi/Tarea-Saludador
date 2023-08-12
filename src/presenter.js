097f142aeca062d90d0711b81774c59c2bd02e63
Al1bi <denilson.fiesta@ucb.edu.bo>
2023-08-11 20:51:59 -0400

    Saludo tomando en cuenta el genero aniadido

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,16 @@
 const button = document.querySelector("#saludar-button");
 const div = document.querySelector("#saludo-div");
 const nombre = document.querySelector("#nombre-usuario");
+const gender = document.querySelector("#gender-selector")
 
 button.addEventListener("click", (event) =>{
 
-  div.innerHTML = "<h1>Hola "+nombre.value+" 🙂</h1>";
+  var vocativo = "";
+
+  if( gender.value == "Masculino") vocativo = "Sr. ";
+  else if( gender.value == "Femenino") vocativo = "Sra. "
+
+  div.innerHTML = "<h1>Hola "+vocativo+nombre.value+" 🙂</h1>";
 
 });
 
