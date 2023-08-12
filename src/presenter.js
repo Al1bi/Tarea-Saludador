2e886451c82fd9909ef879f444866e8d755fc9a6
Al1bi <denilson.fiesta@ucb.edu.bo>
2023-08-11 21:45:59 -0400

    Saludo tomando en cuenta la edad aniadido

--- src/presenter.js
+++ src/presenter.js
@@ -1,14 +1,18 @@
 const button = document.querySelector("#saludar-button");
 const div = document.querySelector("#saludo-div");
 const nombre = document.querySelector("#nombre-usuario");
-const gender = document.querySelector("#gender-selector")
+const gender = document.querySelector("#gender-selector");
+const age = document.querySelector("#age-selector");
 
 button.addEventListener("click", (event) =>{
 
   var vocativo = "";
+  const edad = Number.parseInt(age.value);
 
   if( gender.value == "Masculino") vocativo = "Sr. ";
-  else if( gender.value == "Femenino") vocativo = "Sra. "
+  else if( gender.value == "Femenino") vocativo = "Sra. ";
+
+  if(edad <= 30) vocativo = "";
 
   div.innerHTML = "<h1>Hola "+vocativo+nombre.value+" 🙂</h1>";
 
